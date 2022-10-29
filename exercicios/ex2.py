777495dbbc2a92ccadfa06af9d27dbdfa21c7410
karolina-flores <karolina.lourenco@fatec.sp.gov.br>
2022-10-29 20:47:27 -0300

    Resolução exercicio 2

--- exercicios/ex2.py
+++ exercicios/ex2.py
@@ -1,1 +1,19 @@
+value_salary = float(input ("Informe seu salário fixo: "))
 
+value_sales = float(input ("Informe o valor das vendas: "))
+
+
+def calculate_total_salary (value_salary, value_sales):
+    if value_sales <= 1500:
+        total_salary = (value_sales*5/100)+value_salary
+        print(f"O salário total final do vendedor é: {total_salary}")
+    
+    else:
+        difer_value_sales = value_sales-1500
+        value_sales_basic = value_sales-difer_value_sales
+        total_salary = value_salary+(value_sales_basic*5/100)+(difer_value_sales*7/100)
+        print(f"O salário total final do vendedor é {total_salary}")
+
+calculate_total_salary(value_salary, value_sales)
+
+                 
